a6eac1d63a363d0a7494ab2bbab2725dd792986c
Ilya Sotkov <ilya@sotkov.com>
2022-02-14 18:21:10 +0200

    Add isort checks to CI

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,3 +3,4 @@ set -eux
 poetry run black --check ./tfc_utils ./tests
 poetry run pylint ./tfc_utils ./tests
 poetry run pyright ./tfc_utils ./tests
+poetry run isort ./tfc_utils ./tests --check-only
